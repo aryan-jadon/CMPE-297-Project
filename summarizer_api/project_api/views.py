707adab1847f4c1dffde40303b4c041a2fdb5436
aryan-jadon <aryan.jadon@sjsu.edu>
2022-12-11 14:32:19 -0800

    Refactored API Code

--- summarizer_api/project_api/views.py
+++ summarizer_api/project_api/views.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals
 import json
+import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from transformers import pipeline
-import requests
 from summarizer import Summarizer, TransformerSummarizer
 
 
@@ -16,20 +16,18 @@ class ZeroShortSummarizerAPI(APIView):
                         status=status.HTTP_200_OK)
 
     def post(self, request):
-        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+        try:
+            video_text = request.data['video_text']
+            summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+            response = summarizer(video_text,
+                                  do_sample=False)
 
-        url = "http://127.0.0.1:8001/"
-        payload = {'youtube_url': 'https://www.youtube.com/watch?v=KwlBLxeKj2k&ab_channel=CBSNews'}
-        files = []
-        headers = {}
-        request_response = requests.request("POST", url, headers=headers, data=payload, files=files)
-
-        response = summarizer(json.loads(request_response.text)['video_text'],
-                              do_sample=False)
-
-        return Response({"status": "success",
-                         "results": response},
-                        status=status.HTTP_200_OK)
+            return Response({"status": "success",
+                             "results": response},
+                            status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"status": "failed"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class BertSummarizerAPI(APIView):
@@ -39,19 +37,17 @@ class BertSummarizerAPI(APIView):
                         status=status.HTTP_200_OK)
 
     def post(self, request):
-        bert_model = Summarizer()
+        try:
+            video_text = request.data['video_text']
+            bert_model = Summarizer()
+            bert_summary = ''.join(bert_model(video_text))
 
-        url = "http://127.0.0.1:8001/"
-        payload = {'youtube_url': 'https://www.youtube.com/watch?v=KwlBLxeKj2k&ab_channel=CBSNews'}
-        files = []
-        headers = {}
-        request_response = requests.request("POST", url, headers=headers, data=payload, files=files)
-        print(request_response.text)
-        bert_summary = ''.join(bert_model(json.loads(request_response.text)['video_text']))
-
-        return Response({"status": "success",
-                         "results": bert_summary},
-                        status=status.HTTP_200_OK)
+            return Response({"status": "success",
+                             "results": bert_summary},
+                            status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"status": "failed"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class GPTSummarizerAPI(APIView):
@@ -61,19 +57,17 @@ class GPTSummarizerAPI(APIView):
                         status=status.HTTP_200_OK)
 
     def post(self, request):
-        url = "http://127.0.0.1:8001/"
-        payload = {'youtube_url': 'https://www.youtube.com/watch?v=KwlBLxeKj2k&ab_channel=CBSNews'}
-        files = []
-        headers = {}
-        request_response = requests.request("POST", url, headers=headers, data=payload, files=files)
-        print(request_response.text)
+        try:
+            video_text = request.data['video_text']
+            GPT2_model = TransformerSummarizer(transformer_type="GPT2", transformer_model_key="gpt2-medium")
+            GPT2_summary = ''.join(GPT2_model(video_text))
 
-        GPT2_model = TransformerSummarizer(transformer_type="GPT2", transformer_model_key="gpt2-medium")
-        GPT2_summary = ''.join(GPT2_model(json.loads(request_response.text)['video_text']))
-
-        return Response({"status": "success",
-                         "results": GPT2_summary},
-                        status=status.HTTP_200_OK)
+            return Response({"status": "success",
+                             "results": GPT2_summary},
+                            status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"status": "failed"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class XLNETSummarizerAPI(APIView):
@@ -83,16 +77,15 @@ class XLNETSummarizerAPI(APIView):
                         status=status.HTTP_200_OK)
 
     def post(self, request):
-        url = "http://127.0.0.1:8001/"
-        payload = {'youtube_url': 'https://www.youtube.com/watch?v=KwlBLxeKj2k&ab_channel=CBSNews'}
-        files = []
-        headers = {}
-        request_response = requests.request("POST", url, headers=headers, data=payload, files=files)
-        print(request_response.text)
+        try:
+            video_text = request.data['video_text']
+            xlnet_model = TransformerSummarizer(transformer_type="XLNet",
+                                                transformer_model_key="xlnet-base-cased")
+            xlnet_summary = ''.join(xlnet_model(video_text))
 
-        model = TransformerSummarizer(transformer_type="XLNet", transformer_model_key="xlnet-base-cased")
-        xlnet_summary = ''.join(model(json.loads(request_response.text)['video_text']))
-
-        return Response({"status": "success",
-                         "results": xlnet_summary},
-                        status=status.HTTP_200_OK)
+            return Response({"status": "success",
+                             "results": xlnet_summary},
+                            status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"status": "failed"},
+                            status=status.HTTP_400_BAD_REQUEST)
