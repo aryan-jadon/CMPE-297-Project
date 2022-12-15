31730d0c7325ee9c7fbef08def53f261f3ad9c69
aryan-jadon <aryan.jadon@sjsu.edu>
2022-12-14 19:35:13 -0800

    Added Notebooks

--- notebooks/readme.md
+++ notebooks/readme.md
@@ -1,2 +1,3 @@
 ## Zero Shot Learning Notebook
+
 ### Google Colab Link - https://colab.research.google.com/drive/1XpEB-dLWvy0R5sCNPplpCrv7b66uolVd?usp=sharing
--- notebooks/results.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sklearn.metrics import confusion_matrix, classification_report
-
-results = pd.read_csv("Results.csv")
-print(classification_report(results["actuals"],
-                            results["predictions"]))
